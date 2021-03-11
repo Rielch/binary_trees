4311098a59db61fdcb6be4b8c2f3541961c2252a
Gabriel Prochnau <2288@holbertonschool.com>
2021-03-11 17:03:22 +0000

    Fix betty style for part 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
