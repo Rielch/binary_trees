551dec1849c8f50a4058bdd764145fbc70460aee
Gabriel Prochnau <2288@holbertonschool.com>
2021-03-12 00:40:07 +0000

    Fix part 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,24 @@
 #include "binary_trees.h"
+#include <stdio.h>
+
+/**
+ * calc_exp - calculates the exponent of a number
+ *
+ * @n: number to calculate
+ * @exp: exponent
+ * Return: result
+ */
+
+int calc_exp(int n, int exp)
+{
+	int result;
+
+	if (exp == 0)
+		return (1);
+	result = (calc_exp(n, exp - 1));
+	return (result * n);
+}
+
 
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
@@ -9,89 +29,27 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int height, size, result;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	if (binary_tree_is_full(tree) == 1 && binary_tree_balance(tree) == 0)
-	{
+	height = binary_tree_height(tree);
+	size = binary_tree_size(tree);
+	result = calc_exp(2, height + 1) - 1;
+	if (size == result)
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
-}
-
-/**
- * binary_tree_is_full - checks if a binary tree is full
- *
- * @tree: binary tree to check
- * Return: 1 if is full and 0 otherwise
- */
-
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	int full = 0;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (1);
-	}
-	if (tree->left != NULL)
-	{
-		full += binary_tree_is_full(tree->left);
-	}
-	if (tree->left != NULL)
-	{
-		full += binary_tree_is_full(tree->right);
-	}
-	if (full == 2)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
 }
 
 
 /**
- * binary_tree_balance - Calculates the balance of a binary tree
+ * binary_tree_height - calculates the height of the binary tree
  *
- * @tree: binary tree to calculate
- * Return: Balance of the binary tree
- */
-
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	int balance_l = -1, balance_r = -1;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	if (tree->left != NULL)
-	{
-		balance_l = binary_tree_height(tree->left);
-	}
-	if (tree->right != NULL)
-	{
-		balance_r = binary_tree_height(tree->right);
-	}
-	return (balance_l - balance_r);
-}
-
-/**
- * binary_tree_height - Calculates the height of a binary tree
- *
- * @tree: binary tree to calculate
- * Return: height of the tree
+ * @tree: binary tree
+ * Return: height of the binary tree
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -120,4 +78,31 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (right);
 	}
+}
+
+
+/**
+ * binary_tree_size - Calculates the size of a binary tree
+ *
+ * @tree: node to calculate the size
+ * Return: Size of the binary tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size_l = 0, size_r = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->right != NULL)
+	{
+		size_r = binary_tree_size(tree->right);
+	}
+	if (tree->left != NULL)
+	{
+		size_l = binary_tree_size(tree->left);
+	}
+	return (size_l + size_r + 1);
 }
